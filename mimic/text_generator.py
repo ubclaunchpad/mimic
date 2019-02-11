03b3226aacdfa251d1e6335c36cd88bd0f243796
irvinodjuana <irvino.djuana@gmail.com>
2019-02-10 22:12:18 -0800

    implemented load_text_zip method, extracts .txt contents of zip into same dir and returns text as string

--- mimic/text_generator.py
+++ mimic/text_generator.py
@@ -1,4 +1,5 @@
 from mimic.model.model import Model
+import zipfile, os
 
 class TextGenerator:
     """a user facing class that abstracts away text processing details
@@ -8,10 +9,18 @@ class TextGenerator:
         """initiates the a TextGenerator with a given model type"""
         self.model = model
 
-    def load_text_zip(self, zip):
+    def load_text_zip(self, zip_file_path):
         """prepares a text file for consumption by the model"""
-        raise NotImplementedError
+
+        with zipfile.ZipFile(zip_file_path, "r") as archive:
+            files = [archive.read(name).decode('utf-8') for name in archive.namelist() if name.endswith(".txt")]
+            dir_name = os.path.dirname(zip_file_path)
+            text_string = " ".join(files)
+            archive.extractall(dir_name)
+
+        return text_string
 
     def generate_text(self):
         """generates textual output based on training data"""
         raise NotImplementedError
+
