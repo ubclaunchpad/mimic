f0719e49c181c1a7753b6480a8113027b7b96bec
Jack Yang <jackjiayan@hotmail.com>
2019-02-10 19:36:09 -0800

    added clean shakespear text

---
+++ scripts/clean_shakespear.py
@@ -0,0 +1,19 @@
+import requests
+import re
+import io
+import zipfile
+
+# url = "http://www.gutenberg.org/files/100/100-0.txt"
+url = "https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt"
+text = requests.get(url).text
+
+processed_text = re.sub(("^.*?\\n|\\n\\n.*?\\n|\\n|[A-Z]+:"), " ", text)
+
+mf = io.BytesIO()
+
+with zipfile.ZipFile(mf, mode="w", compression=zipfile.ZIP_DEFLATED) as zf:
+
+    zf.writestr('clean_shakespear.txt', str.encode(processed_text,'utf-8'))
+
+with open("../data/clean_shakespear.zip", "wb") as f: # use `wb` mode
+    f.write(mf.getvalue())
