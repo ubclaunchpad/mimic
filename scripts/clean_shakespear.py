12fdaa130e8bf0d0df63adfc7354f97e8f35cd74
Jack Yang <jackjiayan@hotmail.com>
2019-02-10 19:50:13 -0800

    added comments

--- scripts/clean_shakespear.py
+++ scripts/clean_shakespear.py
@@ -7,12 +7,12 @@ import zipfile
 url = "https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt"
 text = requests.get(url).text
 
+# removed speaker names and newline spacing
 processed_text = re.sub(("^.*?\\n|\\n\\n.*?\\n|\\n|[A-Z]+:"), " ", text)
 
+# saves to zip
 mf = io.BytesIO()
-
 with zipfile.ZipFile(mf, mode="w", compression=zipfile.ZIP_DEFLATED) as zf:
-
     zf.writestr('clean_shakespear.txt', str.encode(processed_text,'utf-8'))
 
 with open("../data/clean_shakespear.zip", "wb") as f: # use `wb` mode
