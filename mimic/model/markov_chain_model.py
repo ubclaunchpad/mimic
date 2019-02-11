de0781862dc29736d6e41a4c091e75a2b0ab6877
Roshan <steradian614@gmail.com>
2019-02-10 19:20:42 -0800

    Just setting up stuff.

--- mimic/model/markov_chain_model.py
+++ mimic/model/markov_chain_model.py
@@ -1,7 +1,30 @@
-from mimic.model.model import Model
+from model import Model
+from collections import defaultdict, Counter
+import random
 
 class MarkovChainModel(Model):
     """A type of model"""
-    pass
 
+    def __init__ (self):
+        this.stateLen = 4
+        this.dict = defaultdict(Counter)
 
+    def learn(self, data):
+        print('Learning...')
+        for i in range(len(data) - this.stateLen):
+            state = data[i, i+this.stateLen]
+            next = data[i_this.statLen]
+            model[state][next] += 1
+        print('Finished Learning')
+        print('--------')
+        for k,v in this.dict.items():
+            print(k,v)
+
+    def predict(self, length):
+        outputState = random.choice(list(model))
+        out = list(outputState)
+        for i in range (0, length):
+            out.extend(random.choices(list(model[state]), model[state].values()))
+            state = state[1:] + out[-1]
+        print(''.join(out))
+            
