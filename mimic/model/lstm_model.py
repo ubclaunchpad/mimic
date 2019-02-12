820a28c8e82005c827bdea1709cb09fcf71f12f5
Frank Jia <jiafrank98@gmail.com>
2019-02-11 21:30:08 -0800

    working model impl

--- mimic/model/lstm_model.py
+++ mimic/model/lstm_model.py
@@ -15,24 +15,16 @@ import string, os
 
 class LSTMModel(Model):
 
+
     def __init__(self):
         self.tokenizer = Tokenizer()
 
+    ''' Create sequences to train the model - each is a sequence of tokens that represent characters '''
     def learn(self, text):
-        # Create sequences to train the model - each is a sequence of tokens that represent characters
-        inp_sequences, total_words = self.__get_sequence_of_tokens(text)
-        predictors, label, max_sequence_len = self.__generate_padded_sequences(inp_sequences, total_words)
-        self.max_sequence_len = max_sequence_len
-        self.model = self.__create_model(max_sequence_len, total_words)
-        self.model.summary()
-        self.model.fit(predictors, label, epochs=100, verbose=5)
 
-    def predict(self):
-        # Arbitrary constants for now
-        return self.__generate_text("Where art thou", 50, self.model, self.max_sequence_len)
-
-    # TODO seq_len and batch_size are currently arbitrary
-    def __get_sequence_of_tokens(self, text, seq_len=100, batch_size=200):
+        # TODO seq_len and batch_size are currently arbitrary
+        SEQ_LEN = 100
+        BATCH_SIZE = 200
 
         # Clean text of weird characters
         text = "".join(v for v in text if v not in string.punctuation).lower()
@@ -40,7 +32,7 @@ class LSTMModel(Model):
 
         # TODO: We need some method of splitting up the input text into chunks of a certain size
         # should be randomized so that we have a good diversity of text
-        corpus = list((clean_txt[0+i:seq_len+i] for i in range(0, len(clean_txt), seq_len)))[0:batch_size]
+        corpus = list((clean_txt[0+i:SEQ_LEN+i] for i in range(0, len(clean_txt), SEQ_LEN)))[0:BATCH_SIZE]
 
         ## Tokenization of corpus
         self.tokenizer.fit_on_texts(corpus)
@@ -52,21 +44,14 @@ class LSTMModel(Model):
                 n_gram_sequence = token_list[:i+1]
                 input_sequences.append(n_gram_sequence)
 
-        return input_sequences, total_words
-
-    # This creates sequences such that all the sequences are the same length
-    def __generate_padded_sequences(self, input_sequences, total_words):
-        max_sequence_len = max([len(x) for x in input_sequences])
-        input_sequences = np.array(pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre'))
-        
+        # This creates sequences such that all the sequences are the same length
+        self.max_sequence_len = max([len(x) for x in input_sequences])
+        input_sequences = np.array(pad_sequences(input_sequences, maxlen=self.max_sequence_len, padding='pre'))
         predictors, label = input_sequences[:,:-1],input_sequences[:,-1]
         label = ku.to_categorical(label, num_classes=total_words)
-        return predictors, label, max_sequence_len
 
-    # Creates the LSTM model to train
-    # TODO Seems like there are lots of arbitrary constants here, experiment to get better results
-    def __create_model(self, max_sequence_len, total_words):
-        input_len = max_sequence_len - 1
+        # Creates the LSTM model to train
+        input_len = self.max_sequence_len - 1
         model = Sequential()
         # Add Input Embedding Layer
         model.add(Embedding(total_words, 10, input_length=input_len))
@@ -76,15 +61,16 @@ class LSTMModel(Model):
         # Add Output Layer
         model.add(Dense(total_words, activation='softmax'))
         model.compile(loss='categorical_crossentropy', optimizer='adam')
-        
-        return model
+        model.fit(predictors, label, epochs=100, verbose=5)
+        self.model = model 
 
-    # Generate output text when given a trained model
-    def __generate_text(self, seed_text, next_words, model, max_sequence_len):
-        for _ in range(next_words):
+    def predict(self):
+        # Arbitrary constants for now
+        seed_text = "where art thou"
+        for _ in range(50):
             token_list = self.tokenizer.texts_to_sequences([seed_text])[0]
-            token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
-            predicted = model.predict_classes(token_list, verbose=0)
+            token_list = pad_sequences([token_list], maxlen=self.max_sequence_len-1, padding='pre')
+            predicted = self.model.predict_classes(token_list, verbose=0)
             
             output_word = ""
             for word,index in self.tokenizer.word_index.items():
@@ -93,5 +79,3 @@ class LSTMModel(Model):
                     break
             seed_text += " "+output_word
         return seed_text
-
-
