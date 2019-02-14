d8f3d9a608fdcb2bba84a47978d7346442a528dd
Roshan <steradian614@gmail.com>
2019-02-14 14:05:07 -0800

    Adde Markov Chain implementation.

--- mimic/model/markov_chain_model.py
+++ mimic/model/markov_chain_model.py
@@ -1,29 +1,44 @@
-from model import Model
-from collections import defaultdict, Counter
+
 import random
+from mimic.model.model import Model
 
 class MarkovChainModel(Model):
     """A type of model"""
 
-    def __init__ (self):
-        this.stateLen = 4
-        this.dict = defaultdict(Counter)
+    def __init__ (self, stateLength):
+        self.order = stateLength
+        self.groupSize = stateLength + 1
+        self.dict = {}
+        self.data = None
 
     def learn(self, data):
+        """Takes in a list of words as an argument
+        and constructs a dictionary based
+        on stateLength provided by the user"""
         print('Learning...')
-        for i in range(0, len(data) - this.stateLen):
-            state = data[i, i+this.stateLen]
-            next = data[i + this.statLen]
-            this.dict[state][next] += 1
+        self.data = data
+
+        for i in range(0,len(data) - self.groupSize):
+            key = tuple(data[i: i +self.order])
+            value = data[i+ self.order]
+
+            if key in self.dict:
+                self.dict[key].append(value)
+            else:
+                self.dict[key] = [value]
+
         print('Finished Learning')
         print('--------')
-        for k,v in this.dict.items():
-            print(k,v)
+
 
     def predict(self, length):
-        outputState = random.choice(list(model))
-        out = list(outputState)
-        for i in range (0, length):
-            out.extend(random.choices(list(model[state]), model[state].values()))
-            state = state[1:] + out[-1]
-        print(''.join(out))
+        """Uses the generated dictionary to create a sentence of specified length"""
+        index = random.randint(0,len(self.data) - self.order)
+        result = self.data[index: index + self.order]
+
+        for i in range(length):
+            state = tuple(result[len(result) - self.order:])
+            next = random.choice(self.dict[state])
+            result.append(next)
+
+        return "".join(result[self.order:])
