325fd6e086dcc9b5431ebf5eb7dd5b074c714afe
r614 <steradian614@gmail.com>
2019-03-02 11:08:29 -0800

    Added logging for Markov, testing.

--- mimic/model/markov_chain_model.py
+++ mimic/model/markov_chain_model.py
@@ -3,6 +3,7 @@
 import random
 from mimic.model.model import Model
 from collections import defaultdict
+import logging
 
 
 class MarkovChainModel(Model):
@@ -19,6 +20,8 @@ class MarkovChainModel(Model):
         self.groupSize = stateLength + 1
         self.dict = defaultdict
         self.data = None
+        logging.basicConfig(format='%(asctime)s %(message)s', filename='markov.log',level=logging.DEBUG)
+        logging.info(format='Markov Model instantiated')
 
     def learn(self, data):
         """
@@ -28,7 +31,7 @@ class MarkovChainModel(Model):
         and constructs a dictionary based
         on stateLength provided by the user.
         """
-        print('Learning...')
+        logging.info('Learning...')
         self.data = data.split()
 
         for i in range(0, len(self.data) - self.groupSize):
@@ -36,8 +39,8 @@ class MarkovChainModel(Model):
             value = self.data[i + self.order]
             self.dict[key].append(value)
 
-        print('Finished Learning')
-        print('--------')
+        logging.info('Finished Learning')
+        logging.info('--------')
 
     def predict(self, length):
         """
@@ -46,6 +49,7 @@ class MarkovChainModel(Model):
         Uses the generated dictionary to create a
         sentence of specified length.
         """
+        logging.info('Predicting')
         index = random.randint(0, len(self.data) - self.order)
         result = self.data[index: index + self.order]
 
