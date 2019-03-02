75086f5c8d949b1cf8e242c4daff1a6a6a99890d
r614 <steradian614@gmail.com>
2019-03-02 13:04:47 -0800

    S T Y L E - 3: Electric Boogaloo

--- mimic/text_generator_factory.py
+++ mimic/text_generator_factory.py
@@ -14,8 +14,9 @@ class TextGeneratorFactory:
     def __init__(self):
         """Initialize a TextGenerator with the given model type."""
         logging.basicConfig(format='%(asctime)s %(message)s',
-                            filename='textGenerator.log',
+                            filename='textgenerator.log',
                             level=logging.DEBUG)
+
 
     def create_markov_chain_text_generator(self):
         """Create a TextGenerator using a markov chain model."""
