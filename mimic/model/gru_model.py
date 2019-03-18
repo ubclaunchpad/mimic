8c3910e642b07d265e62b288638bc7017aae5974
Irvino Djuana <irvino.djuana@gmail.com>
2019-03-17 17:17:59 -0700

    Changed style to comply with pycodestyle

--- mimic/model/gru_model.py
+++ mimic/model/gru_model.py
@@ -2,19 +2,19 @@
 
 from model import Model
 # import mimic.util as utils
-import time
 import os
-import sys
+
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Embedding
+from tensorflow.keras.layers import GRU
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.losses import sparse_categorical_crossentropy
 
 import numpy as np
 import random
 import tensorflow as tf
 tf.enable_eager_execution()
 
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Embedding
-from tensorflow.keras.layers import GRU
-from tensorflow.keras.layers import Dense
 
 # import ssl
 # ssl._create_default_https_context = ssl._create_unverified_context
@@ -22,7 +22,9 @@ from tensorflow.keras.layers import Dense
 
 class GRUModel(Model):
     """Model for text generation using GRU RNN machine learning."""
+
     def __init__(self):
+        """Initialize GRU Model."""
         self.model = None
         self.vocab = None
         self.char2idx = None
@@ -53,7 +55,8 @@ class GRUModel(Model):
         dataset = sequences.map(self.split_input_target)
 
         steps_per_epoch = examples_per_epoch // batch_size
-        dataset = dataset.shuffle(buffer_size).batch(batch_size, drop_remainder=True)
+        dataset = dataset.shuffle(buffer_size).batch(batch_size,
+                                                     drop_remainder=True)
 
         # Length of vocabulary in chars
         vocab_size = len(self.vocab)
@@ -82,20 +85,25 @@ class GRUModel(Model):
             )
 
             # Actual training and fitting of model
-            self.model.fit(dataset.repeat(), epochs=epochs, steps_per_epoch=steps_per_epoch,
+            self.model.fit(dataset.repeat(), epochs=epochs,
+                           steps_per_epoch=steps_per_epoch,
                            callbacks=[checkpoint_callback])
         else:
-            self.model.fit(dataset.repeat(), epochs=epochs, steps_per_epoch=steps_per_epoch)
+            self.model.fit(dataset.repeat(), epochs=epochs,
+                           steps_per_epoch=steps_per_epoch)
 
         # Rebuild model for prediction with same weights but new batch size = 1
         old_weights = self.model.get_weights()
         print(type(old_weights))
-        self.model = self.build_model(vocab_size=vocab_size, embedding_dim=embedding_dim,
-                                      rnn_units=rnn_units, batch_size=1)
+        self.model = self.build_model(vocab_size=vocab_size,
+                                      embedding_dim=embedding_dim,
+                                      rnn_units=rnn_units,
+                                      batch_size=1)
         self.model.set_weights(old_weights)
 
     def load(self, text):
-        #TODO: Implement properly
+        """Load pre-trained weights and build GRU Model."""
+        # TODO: Implement properly
         embedding_dim = 256  # Embedding dimension
         rnn_units = 1024  # Number of rnn units
         batch_size = 1  # Batch size
@@ -107,16 +115,17 @@ class GRUModel(Model):
         self.idx2char = np.array(self.vocab)
         vocab_size = len(self.vocab)
 
-        self.model = self.build_model(vocab_size, embedding_dim, rnn_units, batch_size=batch_size)
+        self.model = self.build_model(vocab_size, embedding_dim,
+                                      rnn_units, batch_size=batch_size)
         self.model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
         self.model.build(tf.TensorShape([1, None]))
         # self.model.summary()
 
     def predict(self):
-        """Generate text using a learned GRU model"""
-        num_generate = 3000         # Number of characters to generate
-        text_generated = []         # Empty string to store our results
-        temperature = 0.7         # temperature : low = more predictable text, high = more surprising text
+        """Generate text using a learned GRU model."""
+        num_generate = 3000     # Number of characters to generate
+        text_generated = []     # Empty list to store our results
+        temperature = 0.7       # Measure of "wildness" of prediction
 
         all_words = self.text.split()
         seed_word = all_words[random.randint(0, len(all_words) - 1)]
@@ -131,9 +140,11 @@ class GRUModel(Model):
             predictions = self.model(input_eval)
             predictions = tf.squeeze(predictions, 0)
 
-            # Using a multinomial distribution to predict the word returned by the model
+            # Using a multinomial distribution to
+            # predict the word returned by the model
             predictions = predictions / temperature
-            predicted_id = tf.multinomial(predictions, num_samples=1)[-1, 0].numpy()
+            predicted_id = tf.multinomial(predictions,
+                                          num_samples=1)[-1, 0].numpy()
 
             # We pass the predicted word as the next input to the model
             # along with the previous hidden state
@@ -145,12 +156,14 @@ class GRUModel(Model):
 
     @staticmethod
     def split_input_target(chunk):
+        """Split text into input and target pairs."""
         input_text = chunk[:-1]
         target_text = chunk[1:]
         return input_text, target_text
 
     @staticmethod
     def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+        """Build the untrained GRU model."""
         if tf.test.is_gpu_available():
             rnn = tf.keras.layers.CuDNNGRU
         else:
@@ -158,12 +171,16 @@ class GRUModel(Model):
             rnn = functools.partial(GRU, recurrent_activation='sigmoid')
 
         model = Sequential()
-        model.add(Embedding(vocab_size, embedding_dim, batch_input_shape=[batch_size, None]))
-        model.add(rnn(rnn_units, return_sequences=True, recurrent_initializer='glorot_uniform', stateful=True))
+        model.add(Embedding(vocab_size, embedding_dim,
+                            batch_input_shape=[batch_size, None]))
+        model.add(rnn(rnn_units, return_sequences=True,
+                      recurrent_initializer='glorot_uniform', stateful=True))
         model.add(Dense(vocab_size))
 
         return model
 
     @staticmethod
     def loss(labels, logits):
-        return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+        """Loss function for GRU Model."""
+        return sparse_categorical_crossentropy(labels, logits,
+                                               from_logits=True)
