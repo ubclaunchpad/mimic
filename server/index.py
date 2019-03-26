f48ed674c7277ad0fa49b3a0e1b60cffd6549568
b5b1b <b5b1b@ugrad.cs.ubc.ca>
2019-03-26 15:44:55 -0700

    changed camelCase to snake_case

--- server/index.py
+++ server/index.py
@@ -7,16 +7,16 @@ import json
 app = Flask(__name__)
 
 
-@app.route('/test-endpoint', methods=['GET'])
-def testEndpoint():
-    """Test endpoint."""
+@app.route('/status', methods=['GET'])
+def status():
+    """status endpoint."""
     return "it's up!"
 
 
 @app.route('/test-model', methods=['GET'])
-def getText():
+def get_text():
     """Use pretrained model to generate text by the requested parameters."""
     data = json.loads(request.data)
-    stringLength = data["stringLength"]
-    seedText = data["seedText"]
+    string_length = data["string_length"]
+    seed_text = data["seed_text"]
     return "deferring this until we have pretrained models!!!"
