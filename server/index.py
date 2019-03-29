74774885e1ee75ce32172ab8c1e89c86658b9d63
b5b1b <b5b1b@ugrad.cs.ubc.ca>
2019-03-29 15:50:44 -0700

    added lstm trump tweets endpoint

--- server/index.py
+++ server/index.py
@@ -4,19 +4,22 @@ from mimic.text_generator import TextGenerator
 from mimic.text_generator_factory import TextGeneratorFactory
 from flask import request
 import json
+from mimic.pretrained_model_factory import PretrainedModelFactory
 app = Flask(__name__)
-
 
 @app.route('/status', methods=['GET'])
 def status():
     """Status endpoint."""
     return "it's up!"
 
-
-@app.route('/test-model', methods=['GET'])
-def get_text():
-    """Use pretrained model to generate text by the requested parameters."""
+@app.route('/lstm_trump_tweets_model', methods=['GET'])
+def get_lstm_trump_text():
+    """Use the LSTM trump tweets pretrained model to generate text by the requested parameters."""
     data = json.loads(request.data)
     string_length = data["string_length"]
     seed_text = data["seed_text"]
-    return "deferring this until we have pretrained models!!!"
+    pretrained_factory = PretrainedModelFactory()
+    LSTM_trump_model = pretrained_factory.create_pretrained_LSTM_trump_tweets_generator()
+    gen_text = LSTM_trump_model.generate_text(seed_text=seed_text, pred_len=int(string_length))
+    return gen_text
+
